0aff528adc27b1bc931c69967c45280001f44745
Jason Chan <jasonpalmspring@gmail.com>
2021-07-23 12:21:04 +1000

    initial commit

---
+++ src/main/java/cognizant/training/junit5/hellojunit5/Greeting.java
@@ -0,0 +1,5 @@
+package cognizant.training.junit5.hellojunit5;
+
+public interface Greeting {
+	String greet(String name);
+}
---
+++ src/main/java/cognizant/training/junit5/hellojunit5/GreetingImpl.java
@@ -0,0 +1,15 @@
+package cognizant.training.junit5.hellojunit5;
+
+public class GreetingImpl implements Greeting {
+
+	@Override
+	public String greet(String name) {
+
+		if (name == null || name.length() == 0) {
+			throw new IllegalArgumentException();
+		}
+
+		return "Hello " + name;
+	}
+
+}
